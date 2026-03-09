53d59cdf1282b8fc1609f8d9aa705c3a0f97b620
Arnie-JeSuis92 <arnehagerup08@gmail.com>
2026-03-09 08:52:52 +0100

    Uploaded code file

---
+++ SystemMonitor.java
@@ -0,0 +1,31 @@
+
+import com.sun.management.OperatingSystemMXBean;
+import java.lang.management.ManagementFactory;
+
+public class SystemMonitor {
+
+    public static void main(String[] args) throws Exception {
+
+        OperatingSystemMXBean osBean
+                = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
+
+        while (true) {
+
+            double cpuLoad = osBean.getCpuLoad() * 100;
+
+            long totalMemory = osBean.getTotalMemorySize();
+            long freeMemory = osBean.getFreeMemorySize();
+            long usedMemory = totalMemory - freeMemory;
+
+            System.out.println("CPU Usage: " + String.format("%.2f", cpuLoad) + "%");
+            System.out.println("Used RAM: " + (usedMemory / 1024 / 1024) + " MB");
+            System.out.println("Free RAM: " + (freeMemory / 1024 / 1024) + " MB");
+            System.out.println("--------------------------");
+
+            Thread.sleep(1000);
+            if (Thread.interrupted())  // Clears interrupted status!
+            throw new InterruptedException();
+
+        }
+    }
+}
